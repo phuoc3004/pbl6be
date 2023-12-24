4173adf8c5318c3fcc5cfe39c55cdcf3d8463f67
phuoc3004 <100799248+phuoc3004@users.noreply.github.com>
2023-12-24 12:26:37 +0700

    CORS

--- src/main/java/com/example/PBL6/Pbl6Application.java
+++ src/main/java/com/example/PBL6/Pbl6Application.java
@@ -2,12 +2,28 @@ package com.example.PBL6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Pbl6Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pbl6Application.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://pbl6.netlify.app") // Add your Netlify app's URL
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
